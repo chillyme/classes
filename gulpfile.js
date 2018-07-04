3fa6a55d630464a23ad7128a16cffb2329684e06
chillyme <511105554@qq.com>
2018-07-04 21:15:23 +0800

    代码美化

--- gulpfile.js
+++ gulpfile.js
@@ -4,18 +4,18 @@ var rename = require("gulp-rename");
 var del = require("del");
 
 var tasks = {
-	minjs: function(){
-		return gulp.src("src/*.js").pipe(minjs()).pipe(rename({ suffix: ".min" })).pipe(gulp.dest("dist"));
-	}
+    minjs: function(){
+        return gulp.src("src/*.js").pipe(minjs()).pipe(rename({ suffix: ".min" })).pipe(gulp.dest("dist"));
+    }
 }
 
 gulp.task("clear", function(callback){
-	return del(["dist/*"], callback);
+    return del(["dist/*"], callback);
 });
 gulp.task("minjs", tasks.minjs);
 
 gulp.task("default", ["clear"], function(){
-	for(var key in tasks){
-		tasks[key]();
-	}
+    for(var key in tasks){
+        tasks[key]();
+    }
 });
